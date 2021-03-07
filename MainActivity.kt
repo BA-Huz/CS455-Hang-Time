16eac21724e3c263cab2ac5fb743b80c1936eaea
BA-Huz <56166683+BA-Huz@users.noreply.github.com>
2021-03-06 20:15:03 -0600

    Add files via upload
    MainActivity.kt and activity_main.xml can now display an invalid login message. Main Activity.kt can now also switch activities into the group list. MainActivity.kt still has a few things left. The landscape mode still needs some work and switching back and forth between portrait and landscape changes the visibility of the incorrect login text. We also need to implement a proper test for if we had a valid login and the button for creating a new account.

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,10 +1,14 @@
 package com.brandon.hangtime
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity()
 {
@@ -14,7 +18,9 @@ class MainActivity : AppCompatActivity()
     private lateinit var usernameEditText : EditText
     private lateinit var passwordEditText : EditText
 
-    // start of call back overrides   **********   start of call back overrides   **********   start of call back overrides   **********
+    private lateinit var invalidLogin : TextView
+
+    // start of call back overrides   **********   start of call back overrides   **********   start of call back overrides
     // onCreate will also call the set functions for thr activity_main.xml widgets
     // and the set functions for event listeners
     override fun onCreate(savedInstanceState: Bundle?)
@@ -24,9 +30,9 @@ class MainActivity : AppCompatActivity()
 
         setButtons()
         setEditTexts()
+        setInvalidLogin()
 
-        setButtonListeners()
-        Log.d("CHEESE   ", "HGKGJVHJHJHVGFCYVUOHBKJFUIGIU")
+        setListeners()
     }
 
     override fun onStart()
@@ -53,7 +59,7 @@ class MainActivity : AppCompatActivity()
     {
         super.onDestroy()
     }
-    // end of call back overrides   **********   end of call back overrides   **********   end of call back overrides   **********
+    // end of call back overrides   **********   end of call back overrides   **********   end of call back overrides
 
 
 
@@ -67,6 +73,11 @@ class MainActivity : AppCompatActivity()
     {
         usernameEditText = findViewById(R.id.usernameEditText)
         passwordEditText = findViewById((R.id.passwordEditText))
+    }
+
+    private fun setInvalidLogin()
+    {
+        invalidLogin = findViewById(R.id.invalidLogin)
     }
 
 
@@ -83,18 +94,59 @@ class MainActivity : AppCompatActivity()
     }
 
 
-    // Code inside of here  ***********************************************************************
-    private fun setButtonListeners()
+    private fun setListeners()
     {
         loginButton.setOnClickListener{
             val userName = getUsernameText()
             val password = getPasswordText()
-            // put code here ***********************************************************************
+
+            // if the user has valid login credentials we will move the the group list activity
+            if(validLoginVerification())
+            {
+                // move to the grouplist activity
+                val intent = Intent(this, GroupList::class.java)
+                startActivity(intent)
+            }
+            else
+            {
+                // display invalid login
+                passwordEditText.setText("")
+                invalidLogin.visibility = TextView.VISIBLE
+            }
 
         }
 
         newUserButton.setOnClickListener{
-            // and here ****************************************************************************
+            // add code here ******************************************************************************************
         }
+
+        // Making the invalid login text no longer visible when you start typing in your
+        // password was strangely difficult because addTextChangedListener is more complicated
+        // then you'd think. The following code snippet comes from
+        // https://www.tutorialkart.com/kotlin-android/android-edittext-on-text-change/
+        passwordEditText.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                invalidLogin.visibility = TextView.INVISIBLE // this line is our own
+                Log.d("HELLO", passwordEditText.text.toString())
+            }
+        })
+    }
+
+
+    private fun validLoginVerification() : Boolean
+    {
+        if(getPasswordText() == "" || getUsernameText() == "")
+            return false
+        else
+            return true // change this later to see if we get a valid login or not ************************************************************
+
     }
 }
