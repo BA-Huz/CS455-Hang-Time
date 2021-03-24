9e13b12c3fd52a8d75d352238c502041325db995
Brandon Alex Huzil <ba.huzil@sasktel.net>
2021-03-24 11:56:26 -0600

    Not much new in this push but the FragmentCalendar.kt now has a listener for when the user selects a differnt date on the personal calendar so later on we can add code to properly populate the event list when selecting different dates.

--- app/src/main/java/com/brandon/hangtime/FragmentPersonalCalendar.kt
+++ app/src/main/java/com/brandon/hangtime/FragmentPersonalCalendar.kt
@@ -2,20 +2,37 @@ package com.brandon.hangtime
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 
 
 class FragmentPersonalCalendar : Fragment()
 {
-
+    private lateinit var calendar : CalendarView
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
-        return inflater!!.inflate(R.layout.fragment_personal_calendar, container, false)
+        var v = inflater.inflate(R.layout.fragment_personal_calendar, container, false)
+
+        setUpCalendar(v)
+
+        return v
     }
 
+    private fun setUpCalendar(v : View)
+    {
+        calendar = v.findViewById(R.id.personalCalendar) as CalendarView
+        calendar?.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            // This function detects when a user selects a different date on the calendar
+            // later on we can put code here to load the correct events into the personal event list
+
+        }
+
+    }
 
 }
