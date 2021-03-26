7e713664abbfa7a772ea5aedbe5709b6d2708b55
Brandon Alex Huzil <ba.huzil@sasktel.net>
2021-03-25 22:35:54 -0600

    Holy Moly, You can now choose the day and time to schedule personal events with all the input checking done as well. Viktor will need to implement submitNewEvent in personal schedule.kt. There is a large comment block to explain what to do

--- app/src/main/java/com/brandon/hangtime/FragmentEventEditor.kt
+++ app/src/main/java/com/brandon/hangtime/FragmentEventEditor.kt
@@ -2,20 +2,267 @@ package com.brandon.hangtime
 
 
 import android.os.Bundle
+import android.text.InputType
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import androidx.fragment.app.Fragment
-
+import java.lang.StringBuilder
 
 
 class FragmentEventEditor : Fragment()
 {
+    // when the user clicks on either edit text it will actually open pop up windows
+    // the edit text is used because it affords clicking
+    private lateinit var startTimeWidget : EditText
+    private lateinit var endTimeWidget: EditText
+    private lateinit var eventNameWidget : EditText
+    //private lateinit var repetitiveRadioGroup : RadioGroup
+    private lateinit var saveButton : Button
+    private lateinit var nonRepeatingRadio : RadioButton
+    private lateinit var dailyRepeatingRadio : RadioButton
+    private lateinit var weeklyRepeatingRadio : RadioButton
+
+    private lateinit var startMessage : String
+    private lateinit var endMessage : String
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
-        return inflater!!.inflate(R.layout.fragment_event_editor, container, false)
+        val v = inflater.inflate(R.layout.fragment_event_editor, container, false)
+
+        setLateInits(v)
+        setListeners()
+
+        return v
+
     }
 
+    // sets up the late inits
+    private fun setLateInits(v : View)
+    {
+        startTimeWidget = v.findViewById(R.id.personalEventStartTimeEditText)
+        // make it so you cannot enter anything into the edit text
+        startTimeWidget.inputType = InputType.TYPE_NULL
+
+        endTimeWidget = v.findViewById(R.id.personalEventEndTimeEditText)
+        endTimeWidget.inputType = InputType.TYPE_NULL
+
+        eventNameWidget = v.findViewById(R.id.personalEventNameEditText)
+
+       // repetitiveRadioGroup = v.findViewById(R.id.repetitiveRadios)
+
+        saveButton = v.findViewById((R.id.savePersonalEventButton))
+
+        nonRepeatingRadio = v.findViewById(R.id.personalEventNonRepeatingRadio)
+        nonRepeatingRadio.isChecked = true
+        dailyRepeatingRadio = v.findViewById(R.id.personalEventDailyRadio)
+        weeklyRepeatingRadio = v.findViewById(R.id.personalEventWeeklyRadio)
+    }
+
+    // sets all the event listeners
+    private fun setListeners()
+    {
+        //set the startTime edittext so when clicked on the parent activity will throw
+        //the correct pop up
+        startTimeWidget.setOnClickListener{
+            (activity as PersonalSchedule).makeTimePopUp(true)
+        }
+
+        endTimeWidget.setOnClickListener{
+            (activity as PersonalSchedule).makeTimePopUp(false)
+        }
+
+        saveButton.setOnClickListener{
+            // the all fields function will already make a toast if its false
+            if(allFieldsFilled())
+            {
+                if(!endTimeIsGreater())
+                    Toast.makeText(activity as PersonalSchedule, "The event must end after it starts", Toast.LENGTH_SHORT).show()
+                else // we can submit the info to the database!!!
+                    submitEvent()
+            }
+        }
+    }
+
+    // this function is intended to be called by the personal schedule for when we are inserting
+    // time into the edit texts
+    internal fun setEdits(startTimeEdit : Boolean, timeMsg : String)
+    {
+        if(startTimeEdit)
+        {
+            startTimeWidget.setText(timeParser(timeMsg))
+            startMessage = timeMsg
+        }
+        else
+        {
+            endTimeWidget.setText(timeParser(timeMsg))
+            endMessage = timeMsg
+        }
+    }
+
+    // this function takes the bundled info to do with date ans time and turns it into a dispalyable readable string
+    private fun timeParser(s : String) : String {
+        val b = StringBuilder()
+        b.append(s)
+        var toReturn: String
+
+        if (b[0] == '0') {
+            toReturn = "JAN "
+        } else if (b[0] == '1' && b[1] == '-') {
+            toReturn = "FEB "
+        } else if (b[0] == '2') {
+            toReturn = "MAR "
+        } else if (b[0] == '3') {
+            toReturn = "APR "
+        } else if (b[0] == '4') {
+            toReturn = "MAY "
+        } else if (b[0] == '5') {
+            toReturn = "JUN "
+        } else if (b[0] == '6') {
+            toReturn = "JUL "
+        } else if (b[0] == '7') {
+            toReturn = "AUG "
+        } else if (b[0] == '8') {
+            toReturn = "SEP "
+        } else if (b[0] == '9') {
+            toReturn = "OCT "
+        } else if (b[0] == '1' && b[1] == '0') {
+            toReturn = "NOV "
+        } else {
+            toReturn = "DEC "
+        }
+
+        //after - is day
+        var i = b.indexOf('-')
+        toReturn += b[i + 1]
+        if (b[i + 2] != '*')
+            toReturn += b[i + 2]
+        toReturn += "   "
+
+        //after * is year
+        i = b.indexOf('*')
+        toReturn += b.substring(i + 1, i + 5).toString() + "    "
+
+
+        //after ; is hour
+        //toReturn += b[i+7]
+        if (b[i + 7] == ':')
+            toReturn += b[i + 6] + ":"
+        else {
+            toReturn += b[i + 6]
+            toReturn += b[i + 7] + ":"
+        }
+
+        // after : is minute
+        i = b.indexOf(':')
+        if (b[i + 2] != '#') {
+            toReturn += b[i + 1]
+            toReturn += b[i + 2]
+        }
+        else
+        {
+            toReturn += "0"
+            toReturn += b[i + 1]
+        }
+
+        // after # is am or pm
+        i = b.indexOf('#')
+        toReturn += " " + b.substring(i+1, i+3)
+
+        return toReturn
+    }
+
+    // returns true if all the fields are filled
+    private fun allFieldsFilled() : Boolean
+    {
+        if(eventNameWidget.text.toString() == "" || (startTimeWidget.text.toString() == "" || endTimeWidget.text.toString() == ""))
+        {
+            Toast.makeText(activity as PersonalSchedule, "All fields must be filled", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        else
+            return true
+    }
+
+    // returns true if the start time of an event is before the end time
+    private fun endTimeIsGreater() : Boolean
+    {
+        val start = startTimeWidget.text.toString()
+        val end = endTimeWidget.text.toString()
+
+        // first lets compare the years
+        // if the start year is less then the end year
+        if(startMessage.substring(startMessage.indexOf('*')+1, startMessage.indexOf(';'))
+                < endMessage.substring(endMessage.indexOf('*')+1, endMessage.indexOf(';')))
+                    return true
+        // if the start year is greater then the end year
+        else if (startMessage.substring(startMessage.indexOf('*')+1, startMessage.indexOf(';'))
+                > endMessage.substring(endMessage.indexOf('*')+1, endMessage.indexOf(';')))
+                    return false
+
+        // now we know the years are equal so lets compare month
+        // if the start month is less then the end month
+        if(startMessage.substring(0, startMessage.indexOf('-'))
+                < endMessage.substring(0, endMessage.indexOf('-')))
+            return true
+        // if the start month is greater then the end month
+        else if (startMessage.substring(0, startMessage.indexOf('-'))
+                > endMessage.substring(0, endMessage.indexOf('-')))
+            return false
+
+        // now we know the months are equal so lets compare days
+        // if the start day is less then the end day
+        if(startMessage.substring(startMessage.indexOf('-')+1, startMessage.indexOf('*'))
+                < endMessage.substring(endMessage.indexOf('-')+1, endMessage.indexOf('*')))
+            return true
+        // if the start day is greater then the end day
+        else if (startMessage.substring(startMessage.indexOf('-')+1, startMessage.indexOf('*'))
+                > endMessage.substring(endMessage.indexOf('-')+1, endMessage.indexOf('*')))
+            return false
+
+        // now we know the days are equal so lets compare AM, PM
+        // if the start day is less then the end day
+        if(startMessage.indexOf('#') + 1 < (endMessage.indexOf('#') + 1))
+            return true
+        else if(startMessage.indexOf('#') + 1 > (endMessage.indexOf('#') + 1))
+            return false
+
+        // now we move onto hour
+        // if the start hour is less then the end hour
+        if(startMessage.substring(startMessage.indexOf(';')+1, startMessage.indexOf(':'))
+                < endMessage.substring(endMessage.indexOf(';')+1, endMessage.indexOf(':')))
+            return true
+        // if the start hour is greater then the end hour
+        else if (startMessage.substring(startMessage.indexOf(';')+1, startMessage.indexOf(':'))
+                > endMessage.substring(endMessage.indexOf(';')+1, endMessage.indexOf(':')))
+            return false
+
+        // deep breath, almost there, finally minute
+        // if the start minute is less then the end minute
+        if(startMessage.substring(startMessage.indexOf(':')+1, startMessage.indexOf('#'))
+                < endMessage.substring(endMessage.indexOf(':')+1, endMessage.indexOf('#')))
+            return true
+        // if the start minute is greater then the end minute
+        else if (startMessage.substring(startMessage.indexOf(':')+1, startMessage.indexOf('#'))
+                > endMessage.substring(endMessage.indexOf(':')+1, endMessage.indexOf('#')))
+            return false
+
+        // if we get to here both dates and times are equal
+        return false
+    }
+
+    // passes nessicary info up to the activity to be submitted to the data bases
+    private fun submitEvent()
+    {
+        if(nonRepeatingRadio.isChecked)
+            (activity as PersonalSchedule).submitNewEvent(startMessage, endMessage, eventNameWidget.text.toString(), 0)
+        else if(dailyRepeatingRadio.isChecked)
+            (activity as PersonalSchedule).submitNewEvent(startMessage, endMessage, eventNameWidget.text.toString(), 1)
+        else
+            (activity as PersonalSchedule).submitNewEvent(startMessage, endMessage, eventNameWidget.text.toString(), 2)
+    }
 
 }
--- app/src/main/java/com/brandon/hangtime/PersonalSchedule.kt
+++ app/src/main/java/com/brandon/hangtime/PersonalSchedule.kt
@@ -1,58 +1,204 @@
 package com.brandon.hangtime
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
-import androidx.fragment.app.Fragment
+import android.widget.FrameLayout
+import java.util.*
+import android.widget.DatePicker
+import android.widget.TimePicker
 
-class PersonalSchedule : AppCompatActivity()
+/*
+    Help for implementing pop ups was taken from Tutorialspoint.com
+    it taught me how to make pop ups and that classes in kotlin can
+    extend a specific abstract function of another class in order to override
+    it
+ */
+
+
+                    // The Personal schedule class will override this function from DatePickerDialog and TimePickerDialog
+class PersonalSchedule : AppCompatActivity(), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener
 {
+
     private lateinit var swapButton : Button
     private var isShowingCalendar = true
+    private lateinit var topFrame: FrameLayout
+    private lateinit var bottomFrame : FrameLayout
+
+    private lateinit var personalCalendarFragment : FragmentPersonalCalendar
+    private lateinit var eventEditorFragment : FragmentEventEditor
+    private lateinit var bottomFragment : FragmentPersonalEventList
+
+    // this variable will determine which fragment to load when we swap fragments
+    private var startTimePicker = true
+
+    // these wll be filled by a calendar instance
+    private var day = 0
+    private var month = 0
+    private var year = 0
+    private var hour = 0
+    private var minute = 0
+
+    // s for saved, these values will be filled by the users input
+    private var sday = 0
+    private var smonth = 0
+    private var syear = 0
+    private var shour = 0
+    private var sminute = 0
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_schedule)
 
-        val personalCalendarFragment = FragmentPersonalCalendar()
-        val eventEditorFragment = FragmentEventEditor()
-        val bottomFragment = FragmentPersonalEventList()
+        setLateInits()
+        setListeners()
 
         //.apply is a scope function so we can write code without adding in blah.blah.blah. to make use of the functions lik replace
         supportFragmentManager.beginTransaction().apply {
             // set the two FrameLayouts to hold these 2 fragments
-            replace(R.id.personalCalendarTopFragment, personalCalendarFragment)
-            replace(R.id.personalEventListFragment, bottomFragment)
+            replace(R.id.personalCalendarTopFragment, personalCalendarFragment, "TOP_FRAG_TAG")
+            replace(R.id.personalEventListFragment, bottomFragment, "BOTTOM_FRAG_TAG")
             commit()
         }
+    }
 
+    // sets the late init widgets and has the fragment objects equal to the correct fragment classes
+    private fun setLateInits()
+    {
+        topFrame = findViewById(R.id.personalCalendarTopFragment)
+        bottomFrame = findViewById(R.id.personalEventListFragment)
         swapButton = findViewById(R.id.personalEventCalendarSwapButton)
+        swapButton.text = "Add Event"
+
+        personalCalendarFragment = FragmentPersonalCalendar()
+        eventEditorFragment = FragmentEventEditor()
+        bottomFragment = FragmentPersonalEventList()
+    }
+
+    // grabs the current time from a calendar instance
+    private fun setCalendarVals()
+    {
+        val c = Calendar.getInstance()
+        day = c.get(Calendar.DAY_OF_MONTH)
+        month = c.get(Calendar.MONTH)
+        year = c.get(Calendar.YEAR)
+        hour = c.get(Calendar.HOUR)
+        minute = c.get(Calendar.MINUTE)
+    }
+
+    //the function name is self explanitory
+    private fun setListeners()
+    {
         swapButton.setOnClickListener{
             if(isShowingCalendar)
             {
                 supportFragmentManager.beginTransaction().apply {
                     // set the two FrameLayouts to hold these 2 fragments
-                    replace(R.id.personalCalendarTopFragment, eventEditorFragment)
+                    replace(R.id.personalCalendarTopFragment, eventEditorFragment, "TOP_FRAG_TAG")
                     commit()
                 }
 
+                swapButton.text = "Back to calendar"
                 isShowingCalendar = false
+               // makePopUp(true)
             }
             else
             {
                 supportFragmentManager.beginTransaction().apply {
                     // set the two FrameLayouts to hold these 2 fragments
-                    replace(R.id.personalCalendarTopFragment, personalCalendarFragment)
+                    replace(R.id.personalCalendarTopFragment, personalCalendarFragment, "TOP_FRAG_TAG")
                     commit()
                 }
-
+                swapButton.text = "Add Event"
                 isShowingCalendar = true
             }
         }
     }
 
+    // An internal method that will be called by fragments of this activity
+    // it creates the pop ups for selecting dates and times
+    internal fun makeTimePopUp(startTimePicker : Boolean)
+    {
+        this.startTimePicker = startTimePicker
+        setCalendarVals()
 
+        //give the info to the dialog constructor as well as a reference to our version of its onDateSet
+        val dpd = DatePickerDialog(this, this, year, month, day)
+        dpd.datePicker.minDate = Calendar.getInstance().timeInMillis
+        dpd.show()
+    // show the dialog pop up
+    }
+
+    // overridden member of the datePickerDialog class
+    // we set it to also make a timePickerDialog
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int)
+    {
+        sday = dayOfMonth
+        syear = year
+        smonth = month
+
+        setCalendarVals()
+        // create a timePicker Dialog
+        var tpd = TimePickerDialog(this, 3,this, hour, minute, false)
+        tpd.show()
+    }
+
+    // overridden member of the datePickerDialog class
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int)
+    {
+        shour = hourOfDay
+        sminute = minute
+
+        //now we also need to send the proper info back to the fragment
+        setFragmentEventEditorEditTexts()
+    }
+
+    // this function will bundle up the info to do with user selected time and date for events
+    // and sends it into the FragmentEventEditor
+    private fun setFragmentEventEditorEditTexts()
+    {
+        var m = "AM"
+        if(shour > 12)
+        {
+            m = "PM"
+            shour -= 12
+        }
+        // I seperated each variable by a unique character
+        var msg = "$smonth-$sday*$year;$shour:$sminute#$m"
+
+        (supportFragmentManager.findFragmentByTag("TOP_FRAG_TAG") as FragmentEventEditor).setEdits(startTimePicker, msg)
+    }
+
+    internal fun submitNewEvent(startMsg : String, endMsg : String, eventName : String, whichRepeating : Int)
+    {
+        //  *****************      HEY VIKTOR      ***********************
+        //  *****************      IVE HAD TOO MUCH COFFEE TODAY      ***********************
+        //  *****************      NOW LET ME EXPLAIN HOW TO USE THESE PARAMETERS      ***********************
+        /*
+            the startMsg and endMsg hold all of the info to do with date and time for when the event starts and ends
+            you dont need to check if they are valid input, ive already done that, at this stage there good
+            whichRepeating shows which radio button is checked
+            0 is non repetitive
+            1 is daily event
+            2 is weekly event
+
+            the information is numbers with the exception of AM or PM that are seperated by unigue characters
+
+      ***      msg = "$smonth-$sday*$year;$shour:$sminute#$m"  take a look at the above function to get an idea     ***
+
+            if you want to extract the hour out of startMsg you can do this
+            val hour = startMsg.substring(startMessage.indexOf(';')+1, startMessage.indexOf(':'))
+            this is because before the hour is the unique char ; and after is :
+
+            however this will not account for am or pm, its in 12 hour not 24 hour time. so to determine if its
+            10:00 in 24 hour time or 22:00 in 24 hour time you can
+            if(startMsg.substring(startMessage.indexOf('#')+1) == "AM")
+                // your in the morning
+
+         */
+    }
 }
